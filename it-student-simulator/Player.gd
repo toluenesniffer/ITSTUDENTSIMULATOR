c8592fc70fdc7c2fc5cac602636e4d5837e6bcd6
zero <156066173+toluenesniffer@users.noreply.github.com>
2025-11-01 21:36:54 +0100

    walking works yay
    he walkin

--- it-student-simulator/Player.gd
+++ it-student-simulator/Player.gd
@@ -2,14 +2,20 @@ extends CharacterBody2D
 
 const SPEED = 150.0
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta) -> void:
 	var direction = Input.get_axis("ui_left", "ui_right")
 	var anim_sprite = $AnimatedSprite2D
 	if direction:
 		anim_sprite.play("chuze")
-		if direction > 0:
-			anim_sprite.flip_h = false
-		elif direction < 0:
-			anim_sprite.flip_h = true
+		velocity.x = direction * SPEED
+	var sprite = $AnimatedSprite2D
+	if direction > 0:
+		sprite.flip_h = false
+	elif direction < 0:
+		sprite.flip_h = true
 	else:
+		velocity.x = 0
+		anim_sprite.play("idle")
+	move_and_slide()
+	
 		
--- it-student-simulator/scenes/button_hallway.gd
+++ it-student-simulator/scenes/button_hallway.gd
@@ -1,4 +1,6 @@
 extends Button
 
+
+
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Hallway.tscn") 
+	get_tree().change_scene_to_file("res://scenes/Hallway.tscn")
