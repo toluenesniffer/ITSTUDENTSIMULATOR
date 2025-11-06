e4626858f39dc4afb7293cc8bbbecdd0834f4d8f
zero <156066173+toluenesniffer@users.noreply.github.com>
2025-11-06 19:18:13 +0100

    FIXED TRANSITIONS YAYYYYYYYYYYYYYYyy
    never again

--- it-student-simulator/Player.gd
+++ it-student-simulator/Player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @onready var sprite = $AnimatedSprite2D
 const SPEED = 300
 
-func _physics_process(_delta) -> void:
+func _physics_process(delta) -> void:
 	var direction = Input.get_axis("ui_left", "ui_right")
 	var anim_sprite = $AnimatedSprite2D
 	if direction:
@@ -21,15 +21,10 @@ func _physics_process(_delta) -> void:
 	
 func _ready():
 	if GlobalScript.player_spawn_side == "left":
-		position.x = 100
+		position.x = 250
+		position.y = 500
 	elif GlobalScript.player_spawn_side == "right":
 		position.x = 900
+		position.y = 500
 	print("Player spawned at: ", position.x)
-	sprite.animation_finished.connect(_on_animation_changed)
-	sprite.animation_changed.connect(_on_animation_changed)
 	
-func _on_animation_changed():
-	if sprite.animation == "walk":
-		sprite.offset = Vector2(-750, -500)
-	elif sprite.animation == "idle":
-		sprite.offset = Vector2(-750, -500)
