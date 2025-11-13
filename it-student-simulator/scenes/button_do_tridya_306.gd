92ea67035d18aaac10f4722184e5eb319922a8a5
zero <156066173+toluenesniffer@users.noreply.github.com>
2025-11-13 19:44:58 +0100

    added 2 more classes and buttons
    oierhgiohrweifohgúowjts=éorl¨)7)87

--- it-student-simulator/scenes/button_do_tridya_306.gd
+++ it-student-simulator/scenes/button_do_tridya_306.gd
@@ -2,5 +2,5 @@ extends Button
 
 
 
-#func _on_pressed() -> void:
-	# get_tree().change_scene_to_file("res://scenes/Classroom.tscn")
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/PcRoomA.tscn")
