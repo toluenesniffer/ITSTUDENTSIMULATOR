b6490cce44219508f0fe34f84ca380b906453afc
zero <156066173+toluenesniffer@users.noreply.github.com>
2025-12-02 11:18:44 +0100

    fixed layout

--- it-student-simulator/scenes/back.gd
+++ it-student-simulator/scenes/back.gd
@@ -4,4 +4,5 @@ extends Button
 
 
 func _on_pressed() -> void:
+	SFXManager.play("res://music/button.mp3")
 	get_tree().change_scene_to_file("res://scenes/Mainscreen.tscn")
--- it-student-simulator/scenes/option.gd
+++ it-student-simulator/scenes/option.gd
@@ -3,4 +3,5 @@ extends Button
 
 
 func _on_pressed() -> void:
+	SFXManager.play("res://music/button.mp3")
 	get_tree().change_scene_to_file("res://scenes/options.tscn")
