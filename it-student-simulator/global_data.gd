95688c672804ade23f664e72b1c06681eff76f1a
zero <156066173+toluenesniffer@users.noreply.github.com>
2025-12-02 17:49:59 +0100

    made it possible to use A and D keys

--- it-student-simulator/global_data.gd
+++ it-student-simulator/global_data.gd
@@ -1,4 +1,14 @@
 extends Node
 
+var is_dialog_active = false
 var player_speed = 300.0
 var master_volume = 1.0
+
+func _ready():
+	Dialogic.timeline_started.connect(_on_dialog_started)
+	Dialogic.timeline_ended.connect(_on_dialog_ended)
+	
+func _on_dialog_started():
+	is_dialog_active = true
+func _on_dialog_ended():
+	is_dialog_active = false
