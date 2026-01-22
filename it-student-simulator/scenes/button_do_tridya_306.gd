98650fb8b4fc9585c29eb171ac1cc28b015ca3e7
zero <156066173+toluenesniffer@users.noreply.github.com>
2026-01-22 16:53:38 +0100

    added web class
    now im waiting for more arts

--- it-student-simulator/scenes/button_do_tridya_306.gd
+++ it-student-simulator/scenes/button_do_tridya_306.gd
@@ -4,4 +4,7 @@ extends Button
 
 func _on_pressed() -> void:
 	SFXManager.play("res://music/dvereotevreni.mp3")
-	get_tree().change_scene_to_file("res://scenes/PcRoomA.tscn")
+	if (GlobalData.current_chapter == 1):
+		get_tree().change_scene_to_file("res://scenes/PcRoomA.tscn")
+	elif (GlobalData.current_chapter == 2):
+		get_tree().change_scene_to_file("res://scenes/PcRoomA_ch2.tscn")
--- it-student-simulator/scenes/pc_room_a_ch_2.gd
+++ it-student-simulator/scenes/pc_room_a_ch_2.gd
@@ -1,23 +1,24 @@
-extends Control
+extends Node2D
 
 var signals_connected = false
-
 func _on_ready() -> void:
 	if !signals_connected:
 		Dialogic.timeline_started.connect(_on_timeline_started)
 		Dialogic.timeline_ended.connect(_on_timeline_ended)
 		signals_connected = true
-	if(Dialogic.VAR.dstartPlayed == false):
-		Dialogic.start("res://dialogicYap/Chapter 2/druhak_start.dtl")
+	if(Dialogic.VAR.dstartPlayed == true and Dialogic.VAR.webisky2Played == false):
+		TaskUI.update_task("SNAŽ SE ZÍSKAT DOBROU ZNÁMKU")
+		Dialogic.start("res://dialogicYap/Chapter 2/webisky_druhak.dtl")
 	else:
 		pass
-		
 func _on_timeline_started():
 	SFXManager.change_music("res://music/ukolhudba.mp3")
 func _on_timeline_ended():
 	TaskUI.show_task("JDI VEN (KLIKNI NA DVEŘE)")
 	SFXManager.change_music("res://music/normalnihudba.mp3")
 
-func _on_button_hallway_pressed() -> void:
-	if (Dialogic.VAR.dstartPlayed == true and Dialogic.VAR.webisky2Played == false):
-		TaskUI.update_task("JDI DO TŘÍDY A306")
+
+func _on_button_pressed() -> void:
+	#if (Dialogic.VAR.webisky2Played == true and Dialogic.VAR.matikaPlayed == false):
+	#	TaskUI.update_task("JDI DO UCEBNY I")
+	SFXManager.play("res://music/dvereotevreni.mp3")
