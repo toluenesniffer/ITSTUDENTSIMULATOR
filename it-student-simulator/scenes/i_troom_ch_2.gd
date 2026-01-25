3839f95b1ece04099c8666d8df80ed90c414d5c9
zero <156066173+toluenesniffer@users.noreply.github.com>
2026-01-25 19:36:28 +0100

    kyb class + some stuff in progress
    trying to make a test

---
+++ it-student-simulator/scenes/i_troom_ch_2.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var signals_connected = false
+
+func _on_ready() -> void:
+	if !signals_connected:
+		Dialogic.timeline_started.connect(_on_timeline_started)
+		Dialogic.timeline_ended.connect(_on_timeline_ended)
+		signals_connected = true
+	if(Dialogic.VAR.webisky2Played == true and Dialogic.VAR.kybisky2Played == false):
+		Dialogic.start("res://dialogicYap/Chapter 2/kybisky_druhak.dtl")
+	else:
+		pass
+		
+func _on_timeline_started():
+	SFXManager.change_music("res://music/ukolhudba.mp3")
+func _on_timeline_ended():
+	TaskUI.show_task("JDI VEN (KLIKNI NA DVEŘE)")
+	SFXManager.change_music("res://music/normalnihudba.mp3")
+
+#func _on_button_hallway_pressed() -> void:
+	#if (Dialogic.VAR.kybisky2Played == true and Dialogic.VAR.cestina2Played == false):
+		#TaskUI.update_task("JDI DO TŘÍDY B209")
