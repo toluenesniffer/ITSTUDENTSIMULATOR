54766a4cd722934890c1da71e9d34403426065fd
zero <156066173+toluenesniffer@users.noreply.github.com>
2026-04-27 19:12:53 +0200

    czech class and idek anymore
    this class is done and so am i.

--- it-student-simulator/scenes/telocvicna.gd
+++ it-student-simulator/scenes/telocvicna.gd
@@ -12,7 +12,7 @@ func _on_ready() -> void:
 	if (Dialogic.VAR.elk2Played == true and Dialogic.VAR.telocvik_d1Played == false):
 		Dialogic.start("res://dialogicYap/Chapter 2/telocvik_druhak.dtl")
 	elif(Dialogic.VAR.prg3Played == true and Dialogic.VAR.telak3_1Played == false):
-		Dialogic.start("res://dialogicYap/Chapter 3/telsk3_1.dtl")
+		Dialogic.start("res://dialogicYap/Chapter 3/telak3_1.dtl")
 	$ProgressBar.visible = false
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 func _on_dialogic_signal(argument: String):
